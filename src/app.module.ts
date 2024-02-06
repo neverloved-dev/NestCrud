2373e387cbd5db8724406a9d5072fff552e8aac5
neverloved-dev <djordjeandjic99@gmail.com>
2024-02-06 19:40:02 +0100

    Connected to PostgresSQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Note\Module } from './note/note/.module';
+import { NotesModule } from './note/note.module';
 import { NoteService } from './note/note.service';
 import { NoteController } from './note/note.controller';
+import { Note } from './note/note.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [Note\Module],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'user1234',
+      database: 'notes',
+      entities: [Note],
+    }),
+    NotesModule],
   controllers: [AppController, NoteController],
   providers: [AppService, NoteService],
 })
--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { NoteController } from '../note.controller';
-import { NoteService } from '../note.service';
+import { NoteController } from './note.controller';
+import { NoteService } from './note.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Note } from '../note.entity';
+import { Note } from './note.entity';
 
 @Module({
   // access Note entity using TypeOrmModule
