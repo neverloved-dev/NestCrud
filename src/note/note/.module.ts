ad738e764cf3073a8ba8da81c7d967286669e467
neverloved-dev <djordjeandjic99@gmail.com>
2024-02-06 18:51:06 +0100

    Added the module code

--- src/note/note/.module.ts
+++ src/note/note/.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { NoteController } from '../note.controller';
+import { NoteService } from '../note.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Note } from '../note.entity';
 
-@Module({})
-export class Note\Module {}
+@Module({
+  // access Note entity using TypeOrmModule
+  imports: [TypeOrmModule.forFeature([Note])],
+  controllers: [NoteController],
+  providers: [NoteService],
+})
+export class NotesModule {}
